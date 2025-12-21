33d9964f8cb11561368b3fad949f70389acbc07b
mexriddin1 <kamoliddinodilov432@gmail.com>
2025-12-21 18:22:17 +0500

    feat: implement dynamic base URL for API requests and add search functionality for cars and tours

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    // allow loading images served from local backend during development
-    domains: ['localhost', '127.0.0.1'],
-    // disable the built-in image optimization during development so Next.js
-    // doesn't attempt to proxy localhost-hosted images (which resolve to
-    // private IPs and are blocked). This makes `next/image` render the
-    // provided src directly.
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.ngrok-free.app",
+        pathname: "/uploads/**",
+      },
+    ],
     unoptimized: true,
   },
 };
