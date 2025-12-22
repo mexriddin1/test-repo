cde8bad193eefc15a606896f57647439c04e23c4
mexriddin1 <kamoliddinodilov432@gmail.com>
2025-12-22 08:12:17 +0500

    fix: remove hardcoded BASE_URL and update API endpoints to relative paths

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
-const BACKEND = process.env.NEXT_PUBLIC_BASE_URL || process.env.BACKEND_URL || "http://185.191.141.85:8080";
+const BACKEND = process.env.NEXT_PUBLIC_BASE_URL || process.env.BACKEND_URL || "https://api.realdreamsuz.com";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'http', hostname: '185.191.141.85', port: '8080', pathname: '/**' },
+      { protocol: 'http', hostname: 'api.realdreamsuz', port: '8080', pathname: '/**' },
       { protocol: 'http', hostname: 'localhost', port: '', pathname: '/**' },
       { protocol: 'http', hostname: '127.0.0.1', port: '', pathname: '/**' },
     ],
